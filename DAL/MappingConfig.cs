a1de1eb0fa573bad21ad966382cb1f93f4af6ead
Espent1004 <espent1004@gmail.com>
2018-04-23 14:09:38 +0200

    Adding maps

---
+++ DAL/MappingConfig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using Model;
+
+namespace DAL.Mapping
+{
+    public class MappingConfig
+    {
+        /*
+        public static MapperConfiguration config = new MapperConfiguration(cfg =>
+        {
+            cfg.CreateMap<DAL.Student, Model.Student>();
+        });
+    */
+        public static void RegisterMaps()
+        {
+            AutoMapper.Mapper.Initialize(config =>
+            {
+                config.CreateMap<Model.Database.Activity, Model.Domain.Activity>();
+
+                config.CreateMap<Model.Database.ActivityType, Model.Domain.ActivityType>();
+
+                config.CreateMap<Model.Database.Users, Model.Domain.Users>()
+                .ForMember(
+                    dest => dest.Password, 
+                    opt => opt.Ignore())
+                .ReverseMap().
+                ForMember(
+                    dest => dest.Password, 
+                    opt => opt.Ignore())
+                .ForMember(
+                    dest => dest.Salt, 
+                    opt => opt.Ignore()
+                    );
+
+                config.CreateMap<Model.Database.Status, Model.Domain.Status>();
+
+                config.CreateMap<Model.Database.Comment, Model.Domain.Comment>();
+
+            });
+        }
+
+    }
+}
