70e09c1fa58af52cb06279a9547077e9c1cf7065
Espent1004 <espent1004@gmail.com>
2018-04-23 13:10:23 +0200

    Adding ActivityType model

---
+++ Model/Database/ActivityType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Database
+{
+    public class ActivityType
+    {
+        public int ActivtyTypeId { get; set; }
+        public String ActivityTypeText { get; set; }
+
+    }
+}
