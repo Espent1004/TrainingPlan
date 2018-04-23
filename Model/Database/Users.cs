908f40c5df87874669cc8b7e617c5cfc29188318
Espent1004 <espent1004@gmail.com>
2018-04-23 13:06:09 +0200

    Setting up project

---
+++ Model/Database/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Database
+{
+    public class Users
+    {
+        public int UserId { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Email { get; set; }
+        public String Phone { get; set; }
+        public byte[] Password { get; set;}
+        public byte[] Salt { get; set; }
+    }
+}
